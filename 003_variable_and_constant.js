07934d604daa881310b4249c9892e085bdd53de4
Augusto Cesar <acs-03@hotmail.com>
2021-05-25 18:30:01 -0300

    variable and constant

---
+++ 003_variable_and_constant.js
@@ -0,0 +1,19 @@
+// Variables
+
+let myName;
+let wife;
+let numberOfChildren;
+
+myName = "João";
+wife = "Maria";
+numberOfChildren = 3;
+
+console.log("Hi! My name is "+myName+", I'm married with "+wife+" and we have "+numberOfChildren+" children.");
+
+//Constants
+
+const NUMBER_ONE = 10;
+const NUMBER_TWO = 3;
+const RESULT = NUMBER_ONE * NUMBER_TWO;
+
+console.log("The result of multiplication is "+RESULT);
