bfafdff2a36f6e04540ad1206b6b291fe27edeba
Augusto Cesar <acs-03@hotmail.com>
2021-05-25 17:17:29 -0300

    comments

---
+++ 002_comments.js
@@ -0,0 +1,23 @@
+ // Example of comment in line
+
+ // console.log("Hello Word!")
+
+console.log("Hello Word!")
+
+/*
+*
+* Example of comment in block
+*
+*/
+
+console.log("Text 1");
+console.log("Text 2");
+console.log("Text 3");
+console.log("Text 4");
+
+/*
+console.log("Text 1");
+console.log("Text 2");
+console.log("Text 3");
+console.log("Text 4");
+*/
