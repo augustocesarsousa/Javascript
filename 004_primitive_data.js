f0895765dc646506b539e5f8560bc1afd0fae3e3
Augusto Cesar <acs-03@hotmail.com>
2021-05-26 16:09:50 -0300

    primitive data

---
+++ 004_primitive_data.js
@@ -0,0 +1,16 @@
+const name1 = 'João'; // String
+const name2 = "José"; // String
+const name3 = `Maria`; // String
+const number1 = 10; // number
+const number2 = 10.5; // number
+let name4; // undefined -> doesn't point to anything in memory
+const name5 = null; // null -> doesn't point to anything in memory
+const aprovedd = true; // boolean
+
+console.log(typeof name1, name1);
+console.log(typeof name2, name2);
+console.log(typeof name3, name3);
+console.log(typeof number1, number1);
+console.log(typeof number2, number2);
+console.log(typeof name5, name5);
+console.log(typeof aprovedd, aprovedd);
