54e4fcbb5d6c837fa6bc01d3416f99d51e6c42b4
Augusto Cesar <acs-03@hotmail.com>
2021-05-27 16:38:00 -0300

    math

---
+++ 009_math.js
@@ -0,0 +1,19 @@
+let number1 = 7.58648;
+
+console.log(Math.floor(number1));
+console.log(Math.ceil(number1));
+console.log(Math.round(number1));
+
+console.log(Math.max(1,5,8,4,3,68,9,49,9665,48,8,16,65,-4));
+console.log(Math.min(1,5,8,4,3,68,9,49,9665,48,8,16,65,-4));
+
+console.log(Math.random());
+console.log((Math.random() * 10).toFixed());
+
+console.log(Math.PI);
+console.log(Math.pow(number1,3)); // number ** 3
+
+
+
+
+
