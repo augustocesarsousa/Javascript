c5c31b05829a0757c60b5bd839c8c27ca9a9171f
Augusto Cesar <acs-03@hotmail.com>
2021-05-31 17:19:20 -0300

    if else

---
+++ 015_if_else.js
@@ -0,0 +1,11 @@
+let hour = 10;
+
+if(hour >= 6 && hour <12){
+    console.log("Good morning!")
+} else if(hour >= 12 && hour < 18){
+    console.log("Good afternoon!")
+} else if(hour >= 18 && hour < 24){
+    console.log("Good evening!")
+} else {
+    console.log("Wrong hour!")
+}
