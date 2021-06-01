aeea04fb82a292e404f455df986e84ccf44669b9
Augusto Cesar <acs-03@hotmail.com>
2021-06-01 13:08:58 -0300

    ternary operator

---
+++ 02_Programmation_logic/017_ternary_operator.js
@@ -0,0 +1,12 @@
+let grade = 8.5;
+let studenty;
+
+// if(grade >= 7){
+//     studenty = "Approved!";
+// } else {
+//     studenty = "Disapproved!"
+// }
+
+studenty = grade >= 7 ? "Approved!" : "Disapproved!";
+
+console.log(studenty);
