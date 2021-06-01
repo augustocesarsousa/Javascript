e109a89dfa1149ecf87a18a1ba44e6b729685a80
Augusto Cesar <acs-03@hotmail.com>
2021-06-01 13:38:02 -0300

    date

---
+++ 02_Programmation_logic/018_date.js
@@ -0,0 +1,13 @@
+let date  = new Date();
+
+console.log(`date.toString() = ${date.toString()}`);
+console.log(`date.getDate() = ${date.getDate()}`);
+console.log(`date.getMonth() = ${date.getMonth()} // 0 - 11`); // 0 - 11
+console.log(`date.getYear() = ${date.getYear()}`);
+console.log(`date.getFullYear() = ${date.getFullYear()}`);
+console.log(`date.getHours() = ${date.getHours()}`);
+console.log(`date.getMinutes() = ${date.getMinutes()}`);
+console.log(`date.getSeconds() = ${date.getSeconds()}`);
+console.log(`date.getMilliseconds() = ${date.getMilliseconds()}`);
+console.log(`date.getDay() = ${date.getDay()} // 0 - 6`); // 0 - 6
+console.log(`date.getTime() = ${date.getTime()}`);
