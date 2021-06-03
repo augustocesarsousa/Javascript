7b9b68500bbdf2de334f69be965246087985dbd5
Augusto Cesar <acs-03@hotmail.com>
2021-06-03 12:08:24 -0300

    for part 2

--- 02_Programmation_logic/020_for.js
+++ 02_Programmation_logic/020_for.js
@@ -1,5 +1,40 @@
-let number = 3;
+// classic for
 
-for(let i = 1; i <=10; i++){
-    console.log(`${number} x ${i} = ${number*i}`);
+console.log(`Classic for`);
+console.log(``);
+
+let colors = ["red", "green", "blue", "yellow"];
+
+for(let i = 1; i < colors.length; i++){
+    console.log(colors[i]);
 }
+
+// for in
+
+console.log(``);
+console.log(`For in`);
+console.log(``);
+
+for(let i in colors){
+    console.log(colors[i]);
+}
+
+// for of
+
+console.log(``);
+console.log(`For of`);
+console.log(``);
+
+for(let value of colors){
+    console.log(value);
+}
+
+// for each
+
+console.log(``);
+console.log(`For each`);
+console.log(``);
+
+colors.forEach(function(e){
+    console.log(e);
+})
