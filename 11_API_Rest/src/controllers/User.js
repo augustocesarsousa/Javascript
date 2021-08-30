8d07cf1d1ff4b52e7085fdd247bad79dbe038532
Augusto Cesar <acs-03@hotmail.com>
2021-08-30 17:09:23 -0300

    API Rest - CRUD user

--- 11_API_Rest/src/controllers/User.js
+++ 11_API_Rest/src/controllers/User.js
@@ -4,11 +4,77 @@ class User {
   async create(req, res) {
     try {
       const newUser = await UserModel.create(req.body);
-      res.json(newUser);
+      return res.json(newUser);
     } catch (e) {
-      res.status(400).json({
+      return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
+    }
+  }
+
+  async index(req, res) {
+    try{
+      const users = await User.findAll();
+      return res.json(users);
+    } catch(e) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try{
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch(e) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try{
+      if(!req.params.id){
+        return res.status(400).json({
+          errors: ['ID not send!'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if(!req.params.id){
+        return res.status(400).json({
+          errors: ['User does not exists!'],
+        });
+      }
+
+      const newDatas = await user.update(req.bory);
+
+      return res.json(newDatas);
+    } catch(e) {
+      return res.json(null);
+    }
+  }
+
+  async delete(req, res) {
+    try{
+      if(!req.params.id){
+        return res.status(400).json({
+          errors: ['ID not send!'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if(!req.params.id){
+        return res.status(400).json({
+          errors: ['User does not exists!'],
+        });
+      }
+
+      await user.destroy();
+
+      return res.json(null);
+    } catch(e) {
+      return res.json(null);
     }
   }
 }
--- 11_API_Rest/src/routes/user.js
+++ 11_API_Rest/src/routes/user.js
@@ -4,5 +4,9 @@ import user from '../controllers/User';
 const router = new Router();
 
 router.post('/', user.create);
+router.get('/', user.index);
+router.get('/:id', user.show);
+router.put('/:id', user.update);
+router.delete('/:id', user.delete);
 
 export default router;
