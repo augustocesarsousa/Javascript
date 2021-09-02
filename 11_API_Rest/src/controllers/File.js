faba8a3213b444b1b7308262220420fdbdc7b9ed
Augusto Cesar <acs-03@hotmail.com>
2021-09-02 18:04:15 -0300

    API Rest - handling uploaded files

--- 11_API_Rest/src/controllers/File.js
+++ 11_API_Rest/src/controllers/File.js
@@ -1,6 +1,19 @@
+import multer from 'multer';
+import multerConfig from '../config/multer';
+
+const upload = multer(multerConfig).single('photo');
+
 class File {
   async store(req, res) {
-    res.json(req.file);
+    return upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({
+          errors: [err.code],
+        });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
 
--- 11_API_Rest/src/routes/file.js
+++ 11_API_Rest/src/routes/file.js
@@ -1,12 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
 
 import file from '../controllers/File';
-import multerConfig from '../config/multer';
 
-const upload = multer(multerConfig);
 const router = new Router();
 
-router.post('/', upload.single('photo'), file.store);
+router.post('/', file.store);
 
 export default router;
